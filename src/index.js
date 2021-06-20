a42dc6bd48a62f7568078acbfbe91798d6e9f8cf
Athos Conde de la Fere <eltecladodesocrates@gmail.com>
2021-06-19 22:50:00 -0400

    redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,70 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
-import AppRouter from './routers/AppRouter'
+// import AppRouter from './routers/AppRouter'
 
 
-ReactDOM.render(
-  <AppRouter />,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <AppRouter />,
+//   document.getElementById('root')
+// );
 
+import { createStore } from 'redux'
+
+const store = createStore((state = { count: 0 }, action) => {
+
+  switch (action.type) {
+    case 'INCREMENT':
+      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
+      return {
+        count: state.count + incrementBy
+      }
+    case 'DECREMENT':
+      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1
+      return {
+        count: state.count - decrementBy
+      }
+    case 'RESET':
+      return {
+        count: 0
+      }
+    case 'SET':
+      return {
+        count: action.count
+      }
+    default:
+      return state
+  }
+
+})
+
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+})
+
+store.dispatch({
+  type: 'INCREMENT',
+  incrementBy: 5
+})
+
+store.dispatch({
+  type: 'INCREMENT'
+})
+
+store.dispatch({
+  type: 'RESET'
+})
+
+store.dispatch({
+  type: 'DECREMENT'
+})
+
+store.dispatch({
+  type: 'DECREMENT',
+  decrementBy: 10
+})
+
+store.dispatch({
+  type: 'SET',
+  count: 101
+})
